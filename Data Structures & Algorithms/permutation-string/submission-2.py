612de2e940f8d605288516dbbd9490bb00402353
Abiral Shrestha <137886729+Abiralstha99@users.noreply.github.com>
2026-07-16 22:08:09 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,25 @@
+# Time complexity: O(n)
+# Space complexity: O(1)  - Because even if the input size grows the max, space is 
+# still 26 key hashmap
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        left = 0 
+        right = len(s1)
+        dict_s1 = dict(Counter(s1)) #O(m)
+        dict_s2 = dict(Counter(s2[left:right])) #O(m)
+        for right in range(len(s1), len(s2)):
+            if (dict_s1 == dict_s2):
+                return True
+            else:
+                dict_s2[s2[right]] = dict_s2.get(s2[right],0) + 1
+                dict_s2[s2[left]] -= 1
+                # We are only removing value by 1, so if any key's value is 0, we remove them
+                # from dict
+                if dict_s2[s2[left]] == 0:
+                    del dict_s2[s2[left]]
+                left += 1
+        # The room doesn't check for the last elem, so in the end we return by checking the 2 dictionaries
+        return dict_s1 == dict_s2
