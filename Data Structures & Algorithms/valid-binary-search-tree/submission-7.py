b2014dfbdfdaf67c84c8fff6365240b83b91c674
Abiral Shrestha <abiralstha333@gmail.com>
2026-07-31 03:05:12 -0400

    Add: valid-binary-search-tree - submission-7

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-7.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # Understand:
+        ''' 1. Left Node < Root < Right Node
+        2. Children must be binary 
+        edge case: Having only one node'''\
+
+        # Match: DFS
+
+        # Plan: 
+        '''Use a helper function to pass '''
+        if root is None:
+            return True
+
+        def helper(root, left, right):
+            if not root:
+                return True
+            if left >= root.val or root.val >= right:
+                return False
+            return helper(root.left, left, root.val) and helper(root.right, root.val, right)
+        return helper(root,float("-inf"), float("inf"))
+
+
+
+
