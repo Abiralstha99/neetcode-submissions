60cf7933fe069e3f1cdd7c8e08dafefe935784e3
Abiral Shrestha <abiralstha333@gmail.com>
2026-08-17 00:47:53 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        '''
+        Understand: 
+        input: list of nums
+        output: different ways of choosing
+
+        choose the number you haven't choosed yet
+
+        Plan:
+        base case : if len(cur) == len(nums):
+        add to the result
+
+        use a boolean use array that marks which element is used or      which is not 
+
+        '''
+
+        res, curr, used = [], [], len(nums)*[False]
+        def dfs(i, curr):
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return 
+            
+            for j in range(len(nums)): 
+                # Append i if not present in used
+                if used[j]: 
+                    continue
+                else:
+                    curr.append(nums[j])
+                    used[j] = True
+                    dfs(j + 1, curr)
+
+                    # To backtrack now pop
+                    curr.pop()
+                    used[j] = False
+        dfs(0,curr)
+        return res
+
+
+
