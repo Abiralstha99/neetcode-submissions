b8bc8599883582dc612fffb64587846a2feb35c7
Abiral Shrestha <abiralstha333@gmail.com>
2026-09-21 13:44:09 -0400

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,36 @@
+'''
+For a graph to be a valid tree:
+1. No cycle
+2. All the components must be connected : Keep a count of visited notes and check if visited == n 
+'''
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        adj_map = defaultdict(list)
+        # COnvert to adj list
+        for node1,node2 in edges: 
+            adj_map[node1].append(node2)
+            adj_map[node2].append(node1)
+        
+        # 
+        visited = set()
+
+        def dfs(node, prev):
+            if node in visited:
+                return False
+            visited.add(node)
+
+            for child in adj_map[node]:
+                if child == prev:
+                    continue
+                if not dfs(child, node):
+                    return False
+            return True
+        
+        return dfs(0,-1) and len(visited) == n
+         
+
+
+
+
+
